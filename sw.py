7034e0db75f47191d488a5747fd657a8283458e3
Nicholas Lee-Hone <nicholas.leehone@gmail.com>
2015-06-27 14:30:48 -0700

    Move all common and utility functions into utilities.py

--- sw.py
+++ sw.py
@@ -1,80 +1,6 @@
-import scipy as sp
 import numpy as np
-import scipy.optimize
-from numpy import sin, cos, abs, pi
-import matplotlib.pyplot as plt
-
-
-def find_roots(func, min_x, max_x, x_steps, args=()):
-    """Find the roots of a function.
-
-    The roots are determined by using sp.optimize.newton on various starting points between min_x and max_x.
-
-    Params:
-      func (function): The function that will be finding the roots of. The function must take the form func(x, arg1, arg2, ...) where x is the variable that we want to find roots for.
-      min_x (float): The minimum value that x can take
-      max_x (float): The maximum value that x can take
-      x_steps (int): The number of slices to create between min_x and max_x. We will try to find a root for each slice, so more slices takes longer to evaluate.
-      args (tuple): The arguments that will be passed to the function
-
-    Returns:
-      roots (list): A list of all the roots found by this function. Will be at most x_steps long. If no roots are found, the list will be empty.
-    """
-    x_trials = np.linspace(min_x, max_x, x_steps)
-    possible_solutions = []
-
-    # Try to find the roots of the first_derivative
-    for x in x_trials:
-        try:
-            possible_solution = sp.optimize.newton(func, x, args=args)
-        except RuntimeError:
-            # The newton root finder could not find a root
-            continue
-        else:
-            possible_solutions.append(possible_solution)
-
-    return possible_solutions
-
-
-def check_root_is_minimum(root, deriv, second_deriv, resolution=0.00001, args=()):
-    """Check if the root of the derivative is a minimum of the function
-
-    Params:
-      root (float): Possible minimum that we want to check.
-      deriv (function): The derivative of the function that we want to find a minimum of. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be putting 'root' into.
-      second_deriv (function): The second derivative of the function that we want to find a minimum of. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be putting 'root' into.
-    resolution (float): How close the second derivative has to be from zero to be considered a valid solution
-    args (tuple): The arguments that will be passed to the derivative and second derivative.
-
-    Returns:
-      bool: Is this a valid minimum or not?
-    """
-    if abs(deriv(root, *args)) < resolution and second_deriv(root, *args) > 0:
-        return True
-
-    return False
-
-
-def find_magnetization(func, deriv, second_deriv, previous_magnetization, min_x, max_x, x_steps, resolution=0.0001, args=()):
-    """Determine the magnetization from the energy function and its first and second derivatives.
-
-    Params:
-      func (function): The energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing against.
-      deriv (function): The derivative of the energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing agains.
-      second_deriv (function): The second derivative of the energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing agains.
-      args (tuple): The arguments to pass to the energy function and its derivatives
-    """
-    possible_minima = find_roots(deriv, min_x, max_x, x_steps, args)
-    minima = []
-    for root in possible_minima:
-        if check_root_is_minimum(root, deriv, second_deriv, resolution=resolution, args=args):
-            minima.append(root)
-
-    diff = abs(previous_magnetization - cos(minima))
-    magnetization = min(zip(diff, cos(minima)))
-
-    return magnetization
-
+from numpy import pi
+from utilities import find_magnetization
 
 def hysteresis_loop(h_min, h_max, num_h_points, energy_func, derivative, second_derivative, min_x=-pi, max_x=pi, x_steps=50, args=()):
     """Calculate the hysteresis loop for the Stoner Wohlfarth model
@@ -104,4 +30,3 @@ def hysteresis_loop(h_min, h_max, num_h_points, energy_func, derivative, second_
         mh_curve.append((h, mag))
 
     return np.array(mh_curve)
-
--- sw_3d.py
+++ sw_3d.py
@@ -1,81 +1,10 @@
-import scipy as sp
 import numpy as np
-import scipy.optimize
-from numpy import sin, cos, abs, pi
+from numpy import pi, cos, sin
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from autodiff import gradient, function
 
-def find_roots(func, min_x, max_x, x_steps, args=()):
-    """Find the roots of a function.
-
-    The roots are determined by using sp.optimize.newton on various starting points between min_x and max_x.
-
-    Params:
-      func (function): The function that will be finding the roots of. The function must take the form func(x, arg1, arg2, ...) where x is the variable that we want to find roots for.
-      min_x (float): The minimum value that x can take
-      max_x (float): The maximum value that x can take
-      x_steps (int): The number of slices to create between min_x and max_x. We will try to find a root for each slice, so more slices takes longer to evaluate.
-      args (tuple): The arguments that will be passed to the function
-
-    Returns:
-      roots (list): A list of all the roots found by this function. Will be at most x_steps long. If no roots are found, the list will be empty.
-    """
-    x_trials = np.linspace(min_x, max_x, x_steps)
-    possible_solutions = []
-
-    # Try to find the roots of the first_derivative
-    for x in x_trials:
-        try:
-            possible_solution = sp.optimize.newton(func, x, args=args)
-        except RuntimeError:
-            # The newton root finder could not find a root
-            continue
-        else:
-            possible_solutions.append(possible_solution)
-
-    return possible_solutions
-
-
-def check_root_is_minimum(root, deriv, second_deriv, resolution=0.00001, args=()):
-    """Check if the root of the derivative is a minimum of the function
-
-    Params:
-      root (float): Possible minimum that we want to check.
-      deriv (function): The derivative of the function that we want to find a minimum of. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be putting 'root' into.
-      second_deriv (function): The second derivative of the function that we want to find a minimum of. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be putting 'root' into.
-    resolution (float): How close the second derivative has to be from zero to be considered a valid solution
-    args (tuple): The arguments that will be passed to the derivative and second derivative.
-
-    Returns:
-      bool: Is this a valid minimum or not?
-    """
-    if abs(deriv(root, *args)) < resolution and second_deriv(root, *args) > 0:
-        return True
-
-    return False
-
-
-def find_magnetization(func, deriv, second_deriv, previous_magnetization, min_x, max_x, x_steps, resolution=0.0001, args=()):
-    """Determine the magnetization from the energy function and its first and second derivatives.
-
-    Params:
-      func (function): The energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing against.
-      deriv (function): The derivative of the energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing agains.
-      second_deriv (function): The second derivative of the energy function. The function should take the form func(x, arg1, arg2, ...) where x is the variable that we will be minimizing agains.
-      args (tuple): The arguments to pass to the energy function and its derivatives
-    """
-    possible_minima = find_roots(deriv, min_x, max_x, x_steps, args)
-    minima = []
-    for root in possible_minima:
-        if check_root_is_minimum(root, deriv, second_deriv, resolution=resolution, args=args):
-            minima.append(root)
-
-    diff = abs(previous_magnetization - cos(minima))
-    magnetization = min(zip(diff, cos(minima)))
-
-    return magnetization
-
+from utilities import find_magnetization
 
 def hysteresis_loop(H, H_dir, Ku_dir, hs, energy_func, derivative, second_derivative, min_x=-pi, max_x=pi, x_steps=50, args=()):
     """Calculate the hysteresis loop for the Stoner Wohlfarth model.
@@ -152,50 +81,3 @@ def hysteresis_loop_anisotropy_distribution(H, H_dir, K_dirs, hs, energy_func, d
     return loops
 
 
-def plot_direction_distribution(directions):
-    # First make sure that the directions are normalized
-    dirs = np.array([v/np.linalg.norm(v) for v in directions])
-
-    fig = plt.figure(figsize=(16,12))
-    ax = fig.add_subplot(121, projection='3d')
-    ax.set_aspect('equal')
-
-    # Make a sphere that has slightly smaller radius that 1 unit
-    u = np.linspace(0, 2*pi, 100)
-    v = np.linspace(0, pi, 100)
-    x = 0.95 * np.outer(np.cos(u), np.sin(v))
-    y = 0.95 * np.outer(np.sin(u), np.sin(v))
-    z = 0.95 * np.outer(np.ones(np.size(u)), np.cos(v))
-    ax.plot_surface(x, y, z, color='w')
-
-    # Plot the directions on the sphere
-    ax.scatter(dirs[:,0], dirs[:,1], dirs[:,2], color='k', s=1)
-
-    ax.azim = 0
-    ax.elev = 0
-
-    ax.set_xlim(-1.1, 1.1)
-    ax.set_ylim(-1.1, 1.1)
-    ax.set_zlim(-1.1, 1.1)
-
-    # Draw a second copy but this time looking from above
-    ax = fig.add_subplot(122, projection='3d')
-    ax.set_aspect('equal')
-
-    ax.plot_surface(x, y, z, color='w')
-
-    # Plot the directions on the sphere
-    ax.scatter(dirs[:,0], dirs[:,1], dirs[:,2], color='k', s=1)
-
-    ax.azim = 0
-    ax.elev = 90
-
-    ax.set_xlim(-1.1, 1.1)
-    ax.set_ylim(-1.1, 1.1)
-    ax.set_zlim(-1.1, 1.1)
-
-    plt.tight_layout()
-    plt.subplots_adjust(wspace=-0.3)
-    plt.show()
-
-    return fig
